fae3aade8274c10738a9b2d2f64ff3720f8f62bc
k3ii <k3i@protonmail.com>
2023-12-14 22:46:26 +0400

    Varialize some parameters

--- variables.tf
+++ variables.tf
@@ -13,3 +13,44 @@ variable "subnet_public2_id" {
 variable "s3_key_prefix" {
   type = string
 }
+
+variable "os_family" {
+  type    = string
+  default = "LINUX"
+}
+
+variable "task_definition_name" {
+  type = string
+}
+
+variable "task_cpu" {
+  type = number
+}
+
+variable "task_memory" {
+  type = number
+}
+
+variable "cpu_arch" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "instance_count" {
+  type = number
+}
